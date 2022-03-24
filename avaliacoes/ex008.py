b9315e43192de9a050552457e46e44bc52c8c0e2
Nícolas Galdino Esmael <nicolasesmael1998@gmail.com>
2022-03-24 15:42:41 -0300

    Pequenos ajustes.

---
+++ avaliacoes/ex008.py
@@ -0,0 +1,16 @@
+'''
+Faça um programa que leia um número indeterminado de valores,
+correspondentes a notas, encerrando a entrada de dados quando for informado
+um valor igual a -1 (que não deve ser armazenado). Após esta entrada de
+dados, faça:
+o Mostre a quantidade de valores que foram lidos;
+o Exiba todos os valores na ordem em que foram informados, um ao lado
+do outro;
+o Exiba todos os valores na ordem inversa à que foram informados, um
+abaixo do outro;
+o Calcule e mostre a soma dos valores;
+o Calcule e mostre a média dos valores;
+o Calcule e mostre a quantidade de valores acima da média calculada;
+o Calcule e mostre a quantidade de valores abaixo de sete;
+o Encerre o programa com uma mensagem;
+'''
