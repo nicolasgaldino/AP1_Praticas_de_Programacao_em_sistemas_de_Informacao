ea7a46af10933b949f49cad5f7476716ad4a1804
Nícolas Galdino Esmael <nicolasesmael1998@gmail.com>
2022-03-30 15:14:05 -0300

    Finalização dos exercícios.
    Ainda estou tentando entender como exibir os valores inversos na questão '08', quando uso o 'reverse()', recebo um retorno 'None'.

--- avaliacoes/ex008.py
+++ avaliacoes/ex008.py
@@ -14,3 +14,26 @@ o Calcule e mostre a quantidade de valores acima da média calculada;
 o Calcule e mostre a quantidade de valores abaixo de sete;
 o Encerre o programa com uma mensagem;
 '''
+notas = []
+olia = notas.sort()
+print('Digite -1 para encerrar o programa')
+while True:
+    nota = float(input('Digite a nota: '))
+    if nota < 0:
+        print('O programa foi encerrado')
+        break
+    notas.append(nota)
+
+media = sum(notas) / len(notas)
+acima = 0
+abaixo = 0
+for n in notas:
+    if n > media:
+        acima += 1
+    if n < 7:
+        abaixo += 1
+print('Foram lidos {} valores.'.format(len(notas)))
+print('Os valores lidos foram: {}'.format(notas), end='\n')
+print('A soma dos valores é: %.2f' % (sum(notas)))
+print('Quantidade de valores acima da média %d' % acima)
+print('Quantidade de valores abaixo de sete: %d' % abaixo)
--- avaliacoes/ex010.py
+++ avaliacoes/ex010.py
@@ -12,3 +12,22 @@ de prestações pagas no dia. O cálculo do valor a ser pago é feito da seguint
 forma. Para pagamentos sem atraso, cobrar o valor da prestação. Quando
 houver atraso, cobrar 3% de multa, mais 0,1% de juros por dia de atraso.
 '''
+def valorPagamento(valor, dias):
+    juros = 0
+    if dias == 0:
+        return valor
+    else:
+        juros = 0.1 * dias
+
+        return valor + ((3 * valor)/100) + juros 
+total = 0
+cont = 0
+while True:
+    valor = float(input('Digite o valor da prestação: '))
+    if valor == 0:
+        break
+    dias = int(input('Digite o número de dias em atraso: '))
+    total += valorPagamento(valor, dias)
+    cont += 1
+print('Quantidade total: %d' % cont)
+print('Valor total das prestações: %.2f' % total)
